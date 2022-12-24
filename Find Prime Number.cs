de04b953748660c6cba591fa4ccb3bdd0a8b4ce9
elzenyy <121347348+elzenyy@users.noreply.github.com>
2022-12-24 16:10:04 +0200

    Add files via upload

---
+++ Find Prime Number.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number_Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Start Number:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The End Number:");
+
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = n1; i <= n2; i++)
+            {
+                bool number = true;
+                if (i == 1)
+                {
+                    number = false;
+
+                }
+                else
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            number = false;
+                            break;
+                        }
+                    }
+                    if (number)
+                    {
+                        Console.WriteLine("the prime number is:" + i);
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+}
+
+
+
---
+++ Find the perfect number.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Perfect_Number_Tester
+{
+    internal class Program
+    {
+        public static int findfactor(int input)
+        {
+            int sum = 0;
+            for (int i = 1; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+
+            return sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter The Start Number");
+            
+             int n1 = int.Parse(Console.ReadLine());
+              
+                     Console.WriteLine("Enter The End Number");
+          
+                               int n2 = int.Parse(Console.ReadLine());
+          
+            for (int i = n1; i < n2; i++)
+            {
+                int result = findfactor(i);
+                if (i == result)
+                {
+                    Console.WriteLine(i+" is a perfect number");
+                }
+            }
+
+        }
+    }
+}
